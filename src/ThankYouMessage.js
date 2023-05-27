7d82b42f12c894e7806e1cb692826ed97d9364c6
Sanjay <sanjay.bscsf19@iba-suk.edu.pk>
2023-05-27 17:42:05 +0500

    Add ThankYouMessage Componenet

---
+++ src/ThankYouMessage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function ThankYouMessage() {
+  return (
+    <div>
+      <h3>Thank you for your vote!</h3>
+      <p>Your participation is appreciated.</p>
+    </div>
+  );
+}
+
+export default ThankYouMessage;
